054d04585c65ffbcbe5428f00370c039570d53ae
witchayada65314669 <witchayadak65@nu.ac.th>
2024-09-06 09:37:49 +0700

    commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,16 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_application_1/screens/lastestRate.dart';
-/*import 'package:flutter_application_1/screens/convert.dart';*/
+import 'package:flutter_application_1/screens/myApp.dart';
+// import 'package:simple_app/screens/convert.dart';
+// import 'package:simple_app/screens/latestRate.dart';
 
 void main() {
   runApp(MaterialApp(
     title: "Exchange Currency",
     home: Scaffold(
       appBar: AppBar(
-        title: const Text("Exchange Currency"),
+        title: const Text("Container Widget"),
       ),
-      body: const LatestRate(),
+      body: const MyApp(),
     ),
   ));
 }
